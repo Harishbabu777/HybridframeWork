82bc65afd768db01f6c70487abfc0d51b97de0dd
Mani3465 <Mr@LAPTOP-AIVEMT1H>
2022-11-11 12:58:34 +0530

    huuuuuuuuuuu

---
+++ SDET42/src/test/java/Practiiiii/Demo.java
@@ -0,0 +1,15 @@
+package Practiiiii;
+
+	public class Demo {
+		public static void main(String[] args) {
+			System.out.println("hii");
+			System.out.println("hello");
+			System.out.println("ramya");
+			System.out.println("pooja");
+			System.out.println("hii");
+			System.out.println("hello");
+			System.out.println("ramya");
+			System.out.println("pooja");
+		}
+		}
+
